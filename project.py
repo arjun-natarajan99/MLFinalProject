f377be041c3b3ee8d13ea5f271fd1eb52f3134fc
arjun-natarajan99 <arjun.nat18@gmail.com>
2020-04-12 13:19:14 -0700

    Starter files

---
+++ project.py
@@ -0,0 +1,12 @@
+"""
+Author      : Arjun Natarajan, Daniel Sealand
+Class       : HMC CS 158
+Date        : 18 February 2020
+Description : Perceptron vs Logistic Regression on a Phoneme Dataset
+"""
+
+# utilities
+import util
+
+# numpy libraries
+import numpy as np
